41103c70005917e9e4d9c162e82d51c1080dd636
ddugky-sparx <150411620+ddugky-sparx@users.noreply.github.com>
2023-12-07 10:43:33 +0530

    sdfxgsd

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.todo,name="main"),
+    path("todo-delete<int:item_id>",views.todo_delete,name="tododelete"),
+    path('clear-items/', views.clear_items, name='clear_items'),
+    path('todo-edit/<int:item_id>', views.edit_todo, name='edit_todo'),
+    path('todo-complete/<int:item_id>', views.todo_complete, name='complete_todo'),
+    
+]
